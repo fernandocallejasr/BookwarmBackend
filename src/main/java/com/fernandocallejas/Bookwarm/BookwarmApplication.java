564c34b78b0150f6ee9de9af43766b541984b88d
Fernando Callejas <ferd.callejas@gmail.com>
2024-10-16 18:51:15 -0600

    Bookwarm Spring Boot project initial setup.

---
+++ src/main/java/com/fernandocallejas/Bookwarm/BookwarmApplication.java
@@ -0,0 +1,13 @@
+package com.fernandocallejas.Bookwarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookwarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandocallejas/Bookwarm/BookwarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandocallejas.Bookwarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookwarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
